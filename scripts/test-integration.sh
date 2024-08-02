486ca15661a27331aeba1e700f56b37cda8e7f6c
whipshout <whipshout@gmail.com>
2024-08-02 06:39:52 +0200

    Clean up

--- scripts/test-integration.sh
+++ scripts/test-integration.sh
@@ -7,7 +7,7 @@ if [[ $directory == *"/scripts" ]]; then
 fi
 
 docker-compose -p tests -f docker/test-integration/docker-compose.yml build --no-cache
-docker-compose -p tests -f docker/test-integration/docker-compose.yml up --force-recreate --abort-on-container-exit --exit-code-from tester
+docker-compose -p tests -f docker/test-integration/docker-compose.yml up --force-recreate --abort-on-container-exit --exit-code-from tester || (echo "Tests failed. Showing logs:" && docker-compose -p tests -f docker/test-integration/docker-compose.yml logs)
 EXIT_CODE=$?
 
 docker-compose -p tests -f docker/test-integration/docker-compose.yml down
