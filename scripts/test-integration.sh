e4a6d8825e15bd63cfe7d1c141ad413b09ef1f2c
whipshout <whipshout@gmail.com>
2024-08-02 05:59:58 +0200

    Fix test-integration script

--- scripts/test-integration.sh
+++ scripts/test-integration.sh
@@ -6,14 +6,14 @@ if [[ $directory == *"/scripts" ]]; then
   cd ..
 fi
 
-docker-compose -f docker/test/docker-compose.yml build
-docker-compose -f docker/test/docker-compose.yml up --force-recreate --abort-on-container-exit
+docker-compose -f docker/test-integration/docker-compose.yml build
+docker-compose -f docker/test-integration/docker-compose.yml up --force-recreate --abort-on-container-exit
 
 CODE=0
-docker-compose -f docker/test/docker-compose.yml ps -q | xargs docker inspect -f '{{ .State.ExitCode }}' | while read code; do
+docker-compose -f docker/test-integration/docker-compose.yml ps -q | xargs docker inspect -f '{{ .State.ExitCode }}' | while read code; do
     if [ "$code" == "1" ]; then
        CODE=-1
     fi
 done
-docker-compose -f docker/test/docker-compose.yml down
+docker-compose -f docker/test-integration/docker-compose.yml down
 exit $CODE
