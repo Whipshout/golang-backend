50831d702097a23d4773742283f55e88e3099ce9
whipshout <whipshout@gmail.com>
2024-08-02 06:42:32 +0200

    Fix test-integration script

--- scripts/test-integration.sh
+++ scripts/test-integration.sh
@@ -7,7 +7,7 @@ if [[ $directory == *"/scripts" ]]; then
 fi
 
 docker-compose -p tests -f docker/test-integration/docker-compose.yml build --no-cache
-docker-compose -p tests -f docker/test-integration/docker-compose.yml up --force-recreate --abort-on-container-exit --exit-code-from tester || (echo "Tests failed. Showing logs:" && docker-compose -p tests -f docker/test-integration/docker-compose.yml logs)
+docker-compose -p tests -f docker/test-integration/docker-compose.yml up --force-recreate --abort-on-container-exit --exit-code-from tester
 EXIT_CODE=$?
 
 docker-compose -p tests -f docker/test-integration/docker-compose.yml down
