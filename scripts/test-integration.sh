0f968d33f9e0124ebde6401a0c952a18fef1fd80
whipshout <whipshout@gmail.com>
2024-08-02 05:55:35 +0200

    Added again exit status code in test-integration script

--- scripts/test-integration.sh
+++ scripts/test-integration.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 set -e
-
 directory=$(pwd)
 if [[ $directory == *"/scripts" ]]; then
   cd ..
 fi
 
 docker-compose -p tests -f docker/test-integration/docker-compose.yml build
-docker-compose -p tests -f docker/test-integration/docker-compose.yml up --force-recreate --abort-on-container-exit
+docker-compose -p tests -f docker/test-integration/docker-compose.yml up --force-recreate --abort-on-container-exit --exit-code-from tester
+EXIT_CODE=$?
 
 docker-compose -p tests -f docker/test-integration/docker-compose.yml down
-
-exit 0
+exit $EXIT_CODE
