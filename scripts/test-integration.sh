16504bd2d06eac74190fa97baa1e158ee08baf63
whipshout <whipshout@gmail.com>
2024-08-02 05:52:41 +0200

    Modified test-integration script

--- scripts/test-integration.sh
+++ scripts/test-integration.sh
@@ -7,8 +7,8 @@ if [[ $directory == *"/scripts" ]]; then
 fi
 
 docker-compose -p tests -f docker/test-integration/docker-compose.yml build
-docker-compose -p tests -f docker/test-integration/docker-compose.yml up --force-recreate --abort-on-container-exit --exit-code-from tester
-EXIT_CODE=$?
+docker-compose -p tests -f docker/test-integration/docker-compose.yml up --force-recreate --abort-on-container-exit
 
 docker-compose -p tests -f docker/test-integration/docker-compose.yml down
-exit $EXIT_CODE
+
+exit 0
